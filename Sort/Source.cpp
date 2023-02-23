902cd78aa147e05211d3c6a294990671b8d679d2
MaksStud <maksgama2@gmail.com>
2023-02-23 23:29:05 +0200

    .

--- Sort/Source.cpp
+++ Sort/Source.cpp
@@ -4,47 +4,37 @@
 using namespace std;
 
 bool SortTest(int* test_array, int* check_array, int size);
+void print(int* arr, int n);
 
 int main() {
 
-    int arr1[6] = { 1, 7, 15, 20, 21, 65 };
-    int arr2[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-    int arr3[5] = {-101, -45, 0, 5, 22};
+    int arr1[7] = {-100, -54, -9, 0, 8, 77, 600};
+    int arr1Not[7] = { 77, -54, -9, 600, 8, -100, 0 };
 
-    int arr1NotSort[6] = { 20, 65, 15, 1, 21, 7 };
-    int arr2NotSort[10] = { 7, 10, 3, 6, 5, 4, 1, 8, 2, 9 };
-    int arr3NotSort[5] = { 0, -101, -45, 22, 5 };
-
-    mergeSort(arr1NotSort, 0, 5 );
-    mergeSort(arr2NotSort, 0, 9 );
-    mergeSort(arr3NotSort, 0, 4 );
-
-    cout << SortTest(arr1NotSort, arr1, 6) << endl;
-    cout << SortTest(arr2NotSort, arr2, 10) << endl;
-    cout << SortTest(arr3NotSort, arr3, 5) << endl;
-
-    int arr4[6] = { 1, 7, 15, 20, 21, 65 };
-    int arr5[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-    int arr6[5] = { -101, -45, 0, 5, 22 };
-
-    int arr4NotSort[6] = { 20, 65, 15, 1, 21, 7 };
-    int arr5NotSort[10] = { 7, 10, 3, 6, 5, 4, 1, 8, 2, 9 };
-    int arr6NotSort[5] = { 0, -101, -45, 22, 5 };
-
-    randomSort(arr4NotSort, 6);
-    randomSort(arr5NotSort, 10);
-    randomSort(arr6NotSort, 5);
-
-    cout << SortTest(arr4NotSort, arr4, 6) << endl;
-    cout << SortTest(arr5NotSort, arr5, 10) << endl;
-    cout << SortTest(arr6NotSort, arr6, 5) << endl;
-  
-
-
-
+    cout << "Enter array: ";
+    print(arr1Not, 7);
     cout << endl;
+
+    randomSort(arr1Not, 7);
+
+    cout << "Sort array: ";
+    print(arr1Not, 7);
+    cout << endl;
+
+
+    cout << "Sort Test: ";
+    cout << SortTest(arr1Not, arr1, 7);
+    cout << endl;
+
     system("pause");
     return 0;
+}
+
+
+void print(int* arr, int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
 }
 
 bool SortTest(int* test_array, int* check_array, int size) {
